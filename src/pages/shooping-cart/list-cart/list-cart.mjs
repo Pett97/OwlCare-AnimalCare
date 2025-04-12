101b4d72263f717e575dd4cfb7998dda40cf0449
Pett97 <henriquepeterson138@gmail.com>
2025-04-12 02:25:24 -0300

    cart 1

--- src/pages/shooping-cart/list-cart/list-cart.mjs
+++ src/pages/shooping-cart/list-cart/list-cart.mjs
@@ -1,6 +1,12 @@
 const URL_CARRINHOS = "http://localhost:3000/carrinho";
 
 document.addEventListener("DOMContentLoaded", async function () {
+  let idPedidoAction;
+
+  function clearLocalStorage() {
+    localStorage.clear();
+  }
+
   async function getPedidos() {
     try {
       const response = await fetch(URL_CARRINHOS);
@@ -16,11 +22,46 @@ document.addEventListener("DOMContentLoaded", async function () {
   }
 
   function listarPedidos(pedidos) {
-    console.log(pedidos);
+    let rowList = document.getElementById("row-list-carts");
+
+    rowList.innerHTML = "";
+
+    pedidos.forEach((pedido) => {
+      const showPedido = document.createElement("div");
+      showPedido.classList.add("col", "s12", "m6", "l3");
+
+      showPedido.innerHTML = `
+                    <div class="card deep-purple lighten-1">
+               <div class="card-content white-text">
+                  <span class="card-title">${pedido.id} - ${pedido.client.clientName}</span>
+                  <p>${pedido.client.email}</p>
+                  <p>${pedido.client.phone}</p>
+                  <p>R$: ${pedido.valorDesconto}</p>
+                  <p>R$: ${pedido.valorCashback}</p>
+               </div>
+               <div class="card-action">
+                  <a data-id="${pedido.id}" onclick="editarPedido(event)">Editar</a>
+                  <a data-id="${pedido.id}>Deletar</a>
+               </div>
+            </div>
+        `;
+      rowList.appendChild(showPedido);
+    });
   }
 
   const data = await getPedidos();
   if (data) {
     listarPedidos(data);
   }
+
+  window.editarPedido = function (event) {
+    let idPedidoAction = event.target.getAttribute("data-id");
+    if(idPedidoAction){
+      localStorage.setItem("idPedidoAction", idPedidoAction);
+
+      window.location.href = "../detail-cart/datail-cart.html";
+    }else{
+      alert("Nao Foi Possivel Abrir Pedido")
+    }
+  };
 });
