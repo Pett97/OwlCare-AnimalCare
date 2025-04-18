dd991fdde5587c21386ab1aef405a757c933f1ee
Pett97 <henriquepeterson138@gmail.com>
2025-04-18 17:16:26 -0300

    navbar responsive ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+  //navabar
+  const elemsNav = document.querySelectorAll('.sidenav');
+  const instancesNav = M.Sidenav.init(elemsNav);
+
+
+  //carrousel
   const elems = document.querySelectorAll('.carousel');
   const instances = M.Carousel.init(elems, {
     fullWidth: true,
